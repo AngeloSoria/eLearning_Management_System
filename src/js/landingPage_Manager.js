0dccda085a4bb0fe4da649f03eccdd5116adc65d
Gelo_ <imangelosoria@gmail.com>
2024-07-26 19:41:39 +0800

    added carousel feature.

--- src/js/landingPage_Manager.js
+++ src/js/landingPage_Manager.js
@@ -1,1 +1,21 @@
 
+let btnNavMenu = document.querySelector(".btn-menu");
+let modalNav = document.querySelector("nav");
+let isNavOpen = false;
+btnNavMenu.addEventListener('click', () => {
+    modalNav.classList.toggle("nav-expanded");
+})
+
+
+let bgCarousel = document.querySelector(".bgCarousel");
+let carouselContent = bgCarousel.querySelector(".content");
+let carouselPaginator = bgCarousel.querySelector(".paginator");
+let carouselBtnControlLeft = carouselPaginator.querySelector("#control-left");
+let carouselBtnControlRight = carouselPaginator.querySelector("#control-right");
+
+carouselBtnControlLeft.addEventListener('click', () => {
+    console.log("Hello World!");
+})
+carouselBtnControlRight.addEventListener('click', () => {
+    console.log("Hello World!");
+})
